aef99414511c103cf897ab5a8a97de9eb5de3919
tajbidtousif <tajbidtousif@gmail.com>
2023-02-02 19:38:18 +0600

    Developing profile section

--- developer_side/lib/ProfileController.dart
+++ developer_side/lib/ProfileController.dart
@@ -1,7 +1,32 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileController with ChangeNotifier{
+  final picker = ImagePicker();
+
+  XFile? _image;
+  XFile get image => _image!;
+
+  Future pickGalleryImage(BuildContext context)async
+  {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery,imageQuality: 100);
+    if(pickedFile!=null)
+      {
+        _image = XFile(pickedFile.path);
+      }
+  }
+
+  Future pickCameraImage(BuildContext context)async
+  {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera,imageQuality: 100);
+    if(pickedFile!=null)
+    {
+      _image = XFile(pickedFile.path);
+    }
+  }
+
   void pickImage(context){
     showDialog(
         context: context,
@@ -14,6 +39,8 @@ class ProfileController with ChangeNotifier{
         children: [
           ListTile(
         onTap: (){
+        pickCameraImage(context);
+        Navigator.pop(context);
 
         },
         leading: Icon(Icons.camera,
@@ -24,7 +51,8 @@ class ProfileController with ChangeNotifier{
 
         ListTile(
         onTap: (){
-
+        pickGalleryImage(context);
+        Navigator.pop(context);
         },
         leading: Icon(Icons.browse_gallery,
         color: Colors.black,
--- developer_side/lib/main.dart
+++ developer_side/lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: welcomePage(),
+      home: lonIn(),
     );
   }
 }
