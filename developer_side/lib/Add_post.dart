09e4fa9e47f91e8e3cec6745b63d21fb90c62256
tajbidtousif <tajbidtousif@gmail.com>
2023-02-15 22:01:08 +0600

    Developing blog part

--- developer_side/lib/Add_post.dart
+++ developer_side/lib/Add_post.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_database/firebase_database.dart';
+import 'package:developer_side/Blog_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class Add_post extends StatefulWidget {
   const Add_post({Key? key}) : super(key: key);
@@ -11,6 +17,10 @@ class Add_post extends StatefulWidget {
 }
 
 class _Add_postState extends State<Add_post> {
+  bool showSpinner = false;
+  final postRef = FirebaseDatabase.instance.ref().child('Posts');
+  firebase_storage.FirebaseStorage storage = firebase_storage.FirebaseStorage.instance;
+  FirebaseAuth _auth = FirebaseAuth.instance;
   File? _image ;
   final picker = ImagePicker();
 
@@ -24,6 +34,7 @@ class _Add_postState extends State<Add_post> {
         _image = File(pickedFile.path);
       }else{
         print('No image selected');
+        toastMessage("No image selected");
       }
     });
   }
@@ -80,106 +91,178 @@ class _Add_postState extends State<Add_post> {
 
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
         appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: Text('Upload Post'),
-            centerTitle: true,
-
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          backgroundColor: Colors.white,
+          title: Text(
+            'Add Experience',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
 
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            children: [
-          InkWell(
-            onTap: (){
-              dialog(context);
-            },
-            child: Center(
-            child: Container(
-            height: MediaQuery.of(context).size.height*.2,
-              width: MediaQuery.of(context).size.width*1,
-      child: _image != null?
-      ClipRect(
-            child: Image.file(
-            _image!.absolute,
-            width: 100,
-            height: 100,
-            fit: BoxFit.fill,
-
-    ),
-    ):
-      Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade400,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            width: 100,
-            height: 100,
-            child: Icon(
-              Icons.camera_alt,
-              color: Colors.blue,
-            ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Column(
+              children: [
+            InkWell(
+              onTap: (){
+                dialog(context);
+              },
+              child: Center(
+              child: Container(
+              height: MediaQuery.of(context).size.height*.2,
+                width: MediaQuery.of(context).size.width*1,
+        child: _image != null?
+        ClipRect(
+              child: Image.file(
+              _image!.absolute,
+              width: 100,
+              height: 100,
+              fit: BoxFit.fill,
 
       ),
-      )
-
-    ),
-          ),
-              SizedBox(height: 30,),
-              Form(
-               child: Column(
-                 children: [
-                   TextFormField(
-                     controller: titleController,
-                     keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        hintText: 'Enter post title',
-                        border: OutlineInputBorder(),
-                        hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                        labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-
-                      ),
-                   ),
-                    SizedBox(height: 30,),
-                   TextFormField(
-                     controller: descriotionController,
-                     keyboardType: TextInputType.text,
-                     minLines: 6,
-                     maxLines: 500,
-                     decoration: InputDecoration(
-                       labelText: 'Description',
-                       hintText: 'Enter post description',
-                       border: OutlineInputBorder(),
-                       hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-                       labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-
-                     ),
-                   ),
-                 ],
-               ),
+      ):
+        Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: 100,
+              height: 100,
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.blue,
               ),
 
-              SizedBox(height: 30,),
+        ),
+        )
 
-               ElevatedButton(
-                  onPressed: (){
+      ),
+            ),
+                SizedBox(height: 30,),
+                Form(
+                 child: Column(
+                   children: [
+                     TextFormField(
+                       controller: titleController,
+                       keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                          hintText: 'Enter post title',
+                          border: OutlineInputBorder(),
+                          hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+                          labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
 
-                  },
-                  child: Text('Upload Post'),
-                )
-    ],
+                        ),
+                     ),
+                      SizedBox(height: 30,),
+                     TextFormField(
+                       controller: descriotionController,
+                       keyboardType: TextInputType.text,
+                       minLines: 6,
+                       maxLines: 500,
+                       decoration: InputDecoration(
+                         labelText: 'Description',
+                         hintText: 'Enter post description',
+                         border: OutlineInputBorder(),
+                         hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+                         labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+
+                       ),
+                     ),
+                   ],
+                 ),
+                ),
+
+                SizedBox(height: 30,),
+
+                 ElevatedButton(
+                    onPressed: ()async{
+                      setState((){
+                        showSpinner = true;
+                      });
+                      try{
+                        int date = DateTime.now().millisecondsSinceEpoch;
+
+                        firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance.ref('Posts/$date');
+                        UploadTask uploadTask = ref.putFile(_image!.absolute) as UploadTask;
+                        await Future.value(uploadTask);
+                        var newUrl = await ref.getDownloadURL();
+
+                        final User? user = _auth.currentUser;
+
+                        postRef.child("Post List").child(date.toString()).set({
+                          "pId": date.toString(),
+                          "pTime": date.toString(),
+                          "pTitle": titleController.text.toString(),
+                          "pDescription": descriotionController.text.toString(),
+                          "pImage": newUrl.toString(),
+                          "UEmail": user!.email.toString(),
+                          "uId": user.uid.toString(),
+
+
+                        }).then((value){
+                          toastMessage("Post Uploaded");
+                          setState((){
+                            showSpinner = false;
+                          });
+
+                        }).onError((error, stackTrace){
+                          toastMessage(error.toString());
+                          setState((){
+                            showSpinner = false;
+                          });
+
+                        });
+
+                      }catch(e){
+
+                        setState((){
+                          showSpinner = false;
+                        });
+
+                        toastMessage(e.toString());
+                      }
+
+                    },
+                    child: Text('Upload Post'),
+                  )
+      ],
+            ),
           ),
+
         ),
 
       ),
-
     );
   }
+
+  void toastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: "Image Uploaded",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey[500],
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+
 }
--- developer_side/lib/Blog_screen.dart
+++ developer_side/lib/Blog_screen.dart
@@ -2,6 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:developer_side/navBar.dart';
+import 'package:flutter/material.dart';
+import 'StudentDashboard.dart';
+import 'logIn.dart';
+import 'signup.dart';
+import 'welcomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'teacherdashboard.dart';
 
 import 'Add_post.dart';
 
@@ -13,48 +25,158 @@ class Blog_screen extends StatefulWidget {
 }
 
 class _Blog_screenState extends State<Blog_screen> {
-  //final dbRef = FirebaseFirestore.instance.collection('Posts');
+
+  final dbRef = FirebaseDatabase.instance.ref().child('Posts');
+  TextEditingController searchController = TextEditingController();
+  String search = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff9fafc),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('News Feed'),
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Carrier Experiences',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           InkWell(
               onTap: (){
-               
-               Navigator.push(context,MaterialPageRoute(
-                   builder: (context)=> Add_post())) ;
+
+                Navigator.push(context,MaterialPageRoute(
+                    builder: (context)=> Add_post())) ;
               },
-              child: Icon(Icons.add)),
+              child: Icon(Icons.add,color: Colors.black)),
           SizedBox(width:20),
-          
+
         ],
       ),
 
-      // body: Column(
-      //   children: [
-      //   Expanded(
-      //       child: FirebaseAnimatedList(
-      //         query: dbRef.child('post_list'),
-      //         itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double>animation,int index){
-      //           return Column(
-      //             children: [
-      //               FadeInImage.assetNetwork(
-      //                   placeholder: "assets/logo.png.png",
-      //                   image: snapshot.value['pImage'],
-      //               )
-      //             ],
-      //           );
-      //
-      //         },
-      //       ),
-      //   )
-      //   ],
-      // ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: searchController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Search with blog title',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder()
+              ),
+              onChanged: (String value){
+                search = value;
+              },
+
+            ),
+          Expanded(
+              child: FirebaseAnimatedList(
+                query: dbRef.child("Post List"),
+                itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double>animation,int index){
+                  dynamic ss = snapshot.value;
+                  String tempTitle = ss['pTitle'];
+
+                  if(searchController.text.isEmpty)
+                    {
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade100,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: FadeInImage.assetNetwork(
+                                  fit: BoxFit.cover,
+                                  width: MediaQuery.of(context).size.width * 1,
+                                  height: MediaQuery.of(context).size.height * .25,
+                                  placeholder: "assets/logo.png.png",
+                                  image: ss['pImage'],
+                                  //image: snapshot.value!.["pImage"] ?? "default url",
+                                ),
+                              ),
+                              SizedBox(height: 10,),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(ss['pTitle'], style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(ss['pDescription'], style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                  else if(tempTitle.toLowerCase().contains(searchController.text.toString()))
+                  {
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: FadeInImage.assetNetwork(
+                                fit: BoxFit.cover,
+                                width: MediaQuery.of(context).size.width * 1,
+                                height: MediaQuery.of(context).size.height * .25,
+                                placeholder: "assets/logo.png.png",
+                                image: ss['pImage'],
+                                //image: snapshot.value!.["pImage"] ?? "default url",
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(ss['pTitle'], style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(ss['pDescription'], style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+                  else{
+                    return Container();
+                  }
+
+
+                },
+              ),
+          )
+          ],
+        ),
+      ),
 
     );
   }
