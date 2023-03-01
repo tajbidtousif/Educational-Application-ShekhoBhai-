7ca15f4f3d500f5c7ef9e3b288ed517f2d6fbef9
tajbidtousif <tajbidtousif@gmail.com>
2023-03-01 14:13:34 +0600

    Developing

--- developer_side/lib/ViewCourse.dart
+++ developer_side/lib/ViewCourse.dart
@@ -1,175 +1,88 @@
-import 'package:developer_side/ComputerGraphics.dart';
+import 'package:developer_side/Oop.dart';
+import 'package:developer_side/TOC.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-import 'Oop.dart';
-import 'TOC.dart';
+import 'ComputerGraphics.dart';
+import 'WebDevelopmentPlaylist.dart';
+import 'Webdevelopment.dart';
 
 class ViewCourse extends StatefulWidget {
-  const ViewCourse({super.key});
+  const ViewCourse({Key? key}) : super(key: key);
 
   @override
   State<ViewCourse> createState() => _ViewCourseState();
 }
 
 class _ViewCourseState extends State<ViewCourse> {
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          backgroundColor: Colors.white,
-          title: Text(
-            'Find your Course',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle:true,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios,color: Colors.black),
+              onPressed: (){
+                Navigator.pop(context);
+              },
             ),
-          ),
-        ),
-        body: Stack(
-          children: <Widget>[
-
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-
-                    Expanded(
-                      child: GridView.count(
-                          crossAxisCount: 2,
-                          childAspectRatio: .90,
-                          crossAxisSpacing: 70,
-                          mainAxisSpacing: 20,
-                          children: <Widget>[
-
-
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => ComputerGraphics()),
-                                );
-
-                              },
-                              child: Container(
-
-                                padding: EdgeInsets.all(5),
-                                decoration: BoxDecoration(
-                                  color: Colors.blue[200],
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      offset: Offset(0, 17),
-                                      blurRadius: 50,
-                                      spreadRadius: -13,
-                                      //color: Colors.blue[100],
-                                    ),
-                                  ],
-                                ),
-                                child: Column(children: <Widget>[
-                                  SizedBox(height: 50),
-                                  Text(
-                                    'Computer Graphics',
-                                    style: TextStyle(
-                                        fontSize: 15, fontWeight: FontWeight.w600),
-                                  ),
-                                ]),
-                              ),
-                            ),
-
-
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => TOC()),
-                                );
-
-                              },
-                              child: Container(
-
-                                padding: EdgeInsets.all(5),
-                                decoration: BoxDecoration(
-                                  color: Colors.deepPurple[300],
-                                  borderRadius: BorderRadius.circular(13),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      offset: Offset(0, 17),
-                                      blurRadius: 50,
-                                      spreadRadius: -13,
-                                      //color: Colors.blue[100],
-                                    ),
-                                  ],
-                                ),
-                                child: Column(children: <Widget>[
-                                  SizedBox(height: 60),
-                                  Text(
-                                    'TOC',
-                                    style: TextStyle(
-                                        fontSize: 30, fontWeight: FontWeight.w600),
-                                  ),
-                                ]),
-                              ),
-                            ),
-
-
-
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => Oop()),
-                                );
-
-                              },
-                              child: Container(
-
-                                padding: EdgeInsets.all(5),
-                                decoration: BoxDecoration(
-                                  color: Colors.green[300],
-                                  borderRadius: BorderRadius.circular(13),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      offset: Offset(0, 17),
-                                      blurRadius: 50,
-                                      spreadRadius: -13,
-                                      //color: Colors.blue[100],
-                                    ),
-                                  ],
-                                ),
-                                child: Column(children: <Widget>[
-                                  SizedBox(height: 60),
-
-                                  Text(
-                                    'OOP',
-                                    style: TextStyle(
-                                        fontSize: 30, fontWeight: FontWeight.w600),
-                                  ),
-                                ]),
-                              ),
-                            ),
-                          ]),
-                    ),
-                  ],
-                ),
+            backgroundColor: Colors.white,
+            title: Text("Find Course",
+              style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,
               ),
+
             ),
-          ],
+          ),
+          body: ListView(
+            children: [
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>WebDevelopmentPlaylist()));
+                },
+                title: Text("Web Development"),
+                subtitle: Text("With Anisul Islam"),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage("https://stiffentechnologies.com/wp-content/uploads/2021/11/Web-Development.jpg"),
+                ),
+                trailing: Icon(Icons.transit_enterexit_outlined),
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ComputerGraphics()));
+                },
+                title: Text("Computer Graphics"),
+                subtitle: Text("With Ebrahim Hossain"),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage("https://media.sciencephoto.com/image/t4780203/400wm/T4780203-Computer_graphics_image_of_4_wire-drawn_spheres.jpg"),
+                ),
+                trailing: Icon(Icons.transit_enterexit_outlined),
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>TOC()));
+                },
+                title: Text("Theory of Computation"),
+                subtitle: Text("With Adil Ahmed Chowdhury"),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage("https://www.analyticssteps.com/backend/media/thumbnail/7112756/6398655_1626243000_Top%20Applications%20of%20Theory%20of%20ComputationArtboard%201.jpg"),
+                ),
+                trailing: Icon(Icons.transit_enterexit_outlined),
+              ),
+              ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Oop()));
+                },
+                title: Text("Object Oriented Programming"),
+                subtitle: Text("With Adil Ahmed Chowdhury"),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage("https://www.developer.com/wp-content/uploads/2021/09/Java-tutorials-300x200.jpg"),
+                ),
+                trailing: Icon(Icons.transit_enterexit_outlined),
+              ),
+            ],
+          ),
         )
     );
-
-
-
   }
 }
-
